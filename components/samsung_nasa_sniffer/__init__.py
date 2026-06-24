6a4c55df3849b6d4e211e4ae97c43e63bf822884
michelebergo <michele.bergo@gmail.com>
2026-06-24 03:52:17 +0200

    fix(component): register sensor and text_sensor platforms separately

--- components/samsung_nasa_sniffer/__init__.py
+++ components/samsung_nasa_sniffer/__init__.py
@@ -8,19 +8,22 @@ SamsungNasaSniffer = samsung_nasa_sniffer_ns.class_(
     "SamsungNasaSniffer", cg.Component, uart.UARTDevice
 )
 
+CONF_SAMSUNG_NASA_SNIFFER_ID = "samsung_nasa_sniffer_id"
 CONF_LAST_PACKET = "last_packet"
 CONF_TOTAL_PACKETS = "total_packets"
 CONF_VALID_PACKETS = "valid_packets"
 CONF_INVALID_PACKETS = "invalid_packets"
 
+SNIFFER_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_SAMSUNG_NASA_SNIFFER_ID): cv.use_id(SamsungNasaSniffer),
+    }
+)
+
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(SamsungNasaSniffer),
         cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-        cv.Optional(CONF_TOTAL_PACKETS): sensor.sensor_schema(),
-        cv.Optional(CONF_VALID_PACKETS): sensor.sensor_schema(),
-        cv.Optional(CONF_INVALID_PACKETS): sensor.sensor_schema(),
-        cv.Optional(CONF_LAST_PACKET): text_sensor.text_sensor_schema(),
     }
 ).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
@@ -31,15 +34,16 @@ async def to_code(config):
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    if CONF_TOTAL_PACKETS in config:
-        sens = await sensor.new_sensor(config[CONF_TOTAL_PACKETS])
-        cg.add(var.set_total_packets_sensor(sens))
-    if CONF_VALID_PACKETS in config:
-        sens = await sensor.new_sensor(config[CONF_VALID_PACKETS])
-        cg.add(var.set_valid_packets_sensor(sens))
-    if CONF_INVALID_PACKETS in config:
-        sens = await sensor.new_sensor(config[CONF_INVALID_PACKETS])
-        cg.add(var.set_invalid_packets_sensor(sens))
-    if CONF_LAST_PACKET in config:
-        sens = await text_sensor.new_text_sensor(config[CONF_LAST_PACKET])
-        cg.add(var.set_last_packet_sensor(sens))
+
+@sensor.register_sensor("samsung_nasa_sniffer", SNIFFER_SCHEMA)
+async def samsung_nasa_sniffer_sensor_to_code(config, var_id):
+    parent = await cg.get_variable(config[CONF_SAMSUNG_NASA_SNIFFER_ID])
+    var = cg.new_Pvariable(var_id, parent)
+    return var
+
+
+@text_sensor.register_text_sensor("samsung_nasa_sniffer", SNIFFER_SCHEMA)
+async def samsung_nasa_sniffer_text_sensor_to_code(config, var_id):
+    parent = await cg.get_variable(config[CONF_SAMSUNG_NASA_SNIFFER_ID])
+    var = cg.new_Pvariable(var_id, parent)
+    return var
